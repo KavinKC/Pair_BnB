8c23715b4b0d76b041036503acaf66ee9fc7486a
KavinKC <Kavin@Kavins-MacBook-Pro.local>
2017-01-03 11:42:15 +0800

    completed user login and editing profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,40 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
+
+  root "users#index"
+  # get 'welcome/index' => 'welcome#index'
+  # root to: 'welcome#index'
+  #
+  # resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  # resource :session, controller: "clearance/sessions", only: [:create]
+  #
+  # resources :users, controller: "users", only: [:create] do
+  #   resource :password,
+  #     controller: "clearance/passwords",
+  #     only: [:create, :edit, :update]
+  # end
+  #
+  # get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  # delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  # get "/sign_up" => "clearance/users#new", as: "sign_up"
+# get "welcome/say_hello" => "welcome#say"
+# ...
+# get "welcome" => "welcome#index"
+ end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +89,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
